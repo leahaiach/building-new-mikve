4c2efee867ffc546aa8503d209d5e36ac4492954
Leah <aiach.leah@gmail.com>
2026-03-08 15:50:49 +0200

    pas atsouma

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 
 export const metadata: Metadata = {
   title: 'קמפיין לבניית מקווה חדש בגבעת מרדכי',
-  description: 'עצומה תושבות לבניית מקווה חדש בגבעת מרדכי',
+  description: 'בניית מקווה חדש בגבעת מרדכי',
 };
 
 export default function RootLayout({
